53fcdd3a5330e31fda176b42732586f4a45f7922
Tushar <tusharimran470@gmail.com>
2024-11-02 19:45:18 +0600

    data get from frontend

--- src/secret.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-
-const serverPort = process.env.port || 5001
-
-module.exports={
-    serverPort
-}
--- src/server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-const {serverPort} = require('./secret');
-
-
-app.listen(serverPort, () => {
-    console.log(`this server is going on port http://localhost:${serverPort}`)
-})
